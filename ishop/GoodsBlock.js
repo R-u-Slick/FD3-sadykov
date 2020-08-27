618454db3f70c7c868f6efaf46232c771f498ec5
R-u-Slick <sadykovruslan91@gmail.com>
2020-08-28 00:41:25 +0300

    commit

--- ishop/GoodsBlock.js
+++ ishop/GoodsBlock.js
@@ -3,20 +3,35 @@
 
   render: function () {
     var goodsCode = [];
+    
     for (var a = 0; a < this.props.goods.length; a++) {
       var product = this.props.goods[a];
-      var productCode = React.DOM.s(
+      var productCode = React.DOM.tr(
         { key: product.code, className: "Product" },
-        React.DOM.span({ className: "Cost" }, product.cost),
-        React.DOM.span({ className: "Stock" }, product.stock),
-        React.DOM.img({ className:"Product-image", src: product.image})
+        React.DOM.td({ className: "Product-name" }, product.name),
+        React.DOM.td({ className: "Cost" }, product.cost),
+        React.DOM.td({ className: "Stock" }, product.stock),
+        React.DOM.td(
+          { className: "Image-cell" },
+          React.DOM.img({ className: "Product-image", src: product.image })
+        )
       );
       goodsCode.push(productCode);
     }
     return React.DOM.div(
       { className: "GoodsBlock" },
       React.DOM.div({ className: "Title" }, this.props.title),
-      React.DOM.div({ className: "Goods-table" }, goodsCode)
+      React.DOM.table(
+        { className: "Goods-table" },
+        React.DOM.tr(
+          { className: "Table-header" },
+          React.DOM.th({ className: "Table-header__content" }, "Наименование"),
+          React.DOM.th({ className: "Table-header__content" }, "Цена, USD"),
+          React.DOM.th({ className: "Table-header__content" }, "Кол-во на складе, шт."),
+          React.DOM.th({ className: "Table-header__content" }, "Изображение"),
+        ),
+        goodsCode
+      )
     );
   },
 });
