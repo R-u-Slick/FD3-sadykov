eb5edfd92b33685dca1fb2b5e5af0b82b467ef60
R-u-Slick <sadykovruslan91@gmail.com>
2020-10-04 00:38:05 +0300

    commit

---
+++ ishop3/component/Product.js
@@ -0,0 +1,50 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Product.css';
+
+class Product extends React.component{
+
+  static propTypes = {
+    code: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    cost: PropTypes.number.isRequired,
+    stock: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    cbSelected: PropTypes.func.isRequired,
+    selectedRowCode: PropTypes.number,
+    cbDeleted: PropTypes.func.isRequired
+  };
+
+
+  rowClicked = (EO) => {
+    EO.stopPropagation();
+    this.props.cbSelected(this.props.code);
+  };
+
+  rowDeleted = (EO) => {
+    EO.stopPropagation();
+    if(confirm('Вы действительно хотите удалить элемент?'))
+    this.props.cbDeleted(this.props.code);
+  };
+
+  render() {
+    return (
+      <tr className={(this.props.code===this.props.selectedRowCode)?"product-selected":"product"} onClick={this.rowClicked}>
+        <td className="Product-name">{this.props.name}</td>
+        <td className="Cost">{this.props.cost}</td>
+        <td className="Stock">{this.props.stock}</td>
+        <td className="Image-cell">
+          <img className="Product-image" src={this.props.image}/>
+        </td>
+        <td className="control-cell">
+          <input type="button" value="удалить" className="delete-button" onClick={this.rowDeleted}/>
+        </td>
+      </tr>
+    )
+  }
+}
+
+
+export default Product;
+
---
+++ ishop3/component/Shop.js
@@ -0,0 +1,70 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Shop.css';
+
+import Product from './Product';
+
+class Shop extends React.Component {
+
+  static propTypes = {
+    title:  PropTypes.string.isRequired,
+    products: PropTypes.arrayOf(
+       PropTypes.shape({
+        code:  PropTypes.number.isRequired,
+        name:  PropTypes.string.isRequired,
+        cost:  PropTypes.number.isRequired,
+        stock:  PropTypes.number.isRequired,
+        image:  PropTypes.string.isRequired
+      })
+    )
+  };
+
+  state = {
+    selectedRowCode: null,
+    currentProducts: this.props.products
+  };
+  
+
+  rowSelected = (code) => {
+    this.setState( {selectedRowCode:code} );
+  };
+
+  rowDeleted = (key) => {
+    var currentRows = this.state.currentProducts.filter(item =>item.code!==key);
+    this.setState({currentProducts: currentRows});
+  }
+
+
+  render () {
+    var productsCode = this.state.currentProducts.map(v =>
+      <Product key={v.code} code={v.code} name={v.name} cost={v.cost} stock={v.stock}
+      image={v.image} cbSelected={this.rowSelected} selectedRowCode={this.state.selectedRowCode}
+      cbDeleted={this.rowDeleted}
+      />
+    )
+    return (
+      <div className='Shop'>
+      <div className='Title'>
+        {this.props.title}
+      </div>
+      <table className='Products-table'>
+        <thead className='Table-header'>
+          <tr>
+            <th>Наименование</th>
+            <th>Цена, USD</th>
+            <th>Кол-во на складе, шт.</th>
+            <th>Изображение</th>
+            <th>Управление</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productsCode}
+        </tbody>
+      </table>
+    </div>
+    );
+  }
+};
+
+export default Shop;
