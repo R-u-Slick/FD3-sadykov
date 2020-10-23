60e3cc614f5c7aff9b466a78aff9f7ecb46aad64
R-u-Slick <sadykovruslan91@gmail.com>
2020-10-23 18:51:08 +0300

    commit

---
+++ br2jsx/component/Br2jsx.js
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Br2jsx.css';
+
+class Br2jsx extends React.Component {
+
+  static propTypes = {
+    text:  PropTypes.string.isRequired,
+  };
+
+  render () {
+    let array = this.props.text.split(/<br.*?>/);
+    let brArray = [];
+    array.forEach((v,i) => {
+      brArray.push(array[i]);
+        if (i<array.length-1) {
+          brArray.push(<br key={i}></br>);
+        }
+      }  
+    )
+    return (
+      <div className="br2jsx">
+        {brArray}
+      </div>
+    )
+  }
+};
+
+export default Br2jsx;
